41c6003cce747c8c63e29de9d756dad2339c68bc
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-01 12:38:16 +0300

    feat: add list files functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,26 @@
+import { existsSync, readdir } from 'fs'
+import path from 'path'
+
+const defPath = path.resolve('fs')
+
 export const list = async () => {
-  // Write your code here
+  try {
+    if (!existsSync(`${defPath}/files`)) {
+      throw new Error('FS operation failed')
+    } else {
+      readdir(`${defPath}/files/`, (err, files) => {
+        if (err) throw err
+
+        files.forEach((file) => {
+          console.log('--------------------')
+          console.log(file)
+        })
+        console.log('--------------------')
+      })
+    }
+  } catch (err) {
+    console.error(err.message)
+  }
 }
+
+list()
