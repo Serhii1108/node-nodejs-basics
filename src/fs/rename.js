cbf842248136566efc568bbb08eb84adb7eff8a6
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-01 12:21:56 +0300

    feat: add rename file functionality

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,27 @@
+import { existsSync, rename as changeName } from 'fs'
+import path from 'path'
+
+const defPath = path.resolve('fs')
+
 export const rename = async () => {
-  // Write your code here
+  try {
+    if (
+      !existsSync(`${defPath}/files/wrongFilename.txt`) ||
+      existsSync(`${defPath}/files/properFilename.md`)
+    ) {
+      throw new Error('FS operation failed')
+    } else {
+      changeName(
+        `${defPath}/files/wrongFilename.txt`,
+        `${defPath}/files/properFilename.md`,
+        (err) => {
+          if (err) throw err
+        }
+      )
+    }
+  } catch (err) {
+    console.error(err.message)
+  }
 }
+
+rename()
