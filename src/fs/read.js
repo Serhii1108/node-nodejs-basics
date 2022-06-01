b4f11e20cc2d907f99ddf4c3142927dca6dce770
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-01 12:46:06 +0300

    feat: add read file functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,28 @@
+import { existsSync, readFile } from 'fs'
+import path from 'path'
+
+const defPath = path.resolve('fs')
+
 export const read = async () => {
-  // Write your code here
+  try {
+    if (!existsSync(`${defPath}/files/fileToRead.txt`)) {
+      throw new Error('FS operation failed')
+    } else {
+      readFile(
+        `${defPath}/files/fileToRead.txt`,
+        { encoding: 'utf8' },
+        (err, data) => {
+          if (err) throw err
+
+          console.log('--------------')
+          console.table(data)
+          console.log('--------------')
+        }
+      )
+    }
+  } catch (err) {
+    console.error(err.message)
+  }
 }
+
+read()
