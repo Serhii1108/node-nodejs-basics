dc948b7c79c0954eacab4f89d2f9ce6d947576d3
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-01 12:27:43 +0300

    feat: add delete file functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,20 @@
+import { rm, existsSync } from 'fs'
+import path from 'path'
+
+const defPath = path.resolve('fs')
+
 export const remove = async () => {
-  // Write your code here
+  try {
+    if (!existsSync(`${defPath}/files/fileToRemove.txt`)) {
+      throw new Error('FS operation failed')
+    } else {
+      rm(`${defPath}/files/fileToRemove.txt`, (err) => {
+        if (err) throw err
+      })
+    }
+  } catch (err) {
+    console.error(err.message)
+  }
 }
+
+remove()
