4b18667481cf8fa398a13d916362e261bb54e94d
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-01 11:49:46 +0300

    feat: add create file functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,24 @@
+import { appendFile, existsSync } from 'fs'
+import path from 'path'
+
+const defPath = path.resolve('fs')
+
 export const create = async () => {
-  // Write your code here
+  try {
+    if (existsSync(`${defPath}/files/fresh.txt`)) {
+      throw new Error('FS operation failed')
+    } else {
+      appendFile(
+        `${defPath}/files/fresh.txt`,
+        'I am fresh and young',
+        (err) => {
+          if (err) throw err
+        }
+      )
+    }
+  } catch (err) {
+    console.error(err.message)
+  }
 }
+
+create()
