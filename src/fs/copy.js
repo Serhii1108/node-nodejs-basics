27aeb3c02ee06d750a6b8a3b079c2ba983009827
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-01 12:11:52 +0300

    feat: add copy files functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,36 @@
+import { copyFile, existsSync, mkdirSync, readdir } from 'fs'
+import path from 'path'
+
+const defPath = path.resolve('fs')
+
 export const copy = async () => {
-  // Write your code here
+  try {
+    if (
+      !existsSync(`${defPath}/files`) ||
+      existsSync(`${defPath}/files_copy`)
+    ) {
+      throw new Error('FS operation failed')
+    } else {
+      mkdirSync(`${defPath}/files_copy/`)
+
+      readdir(`${defPath}/files/`, (err, files) => {
+        if (err) {
+          throw err
+        }
+        files.forEach((file) => {
+          copyFile(
+            `${defPath}/files/${file}`,
+            `${defPath}/files_copy/${file}`,
+            (err) => {
+              if (err) throw err
+            }
+          )
+        })
+      })
+    }
+  } catch (err) {
+    console.error(err.message)
+  }
 }
+
+copy()
