7dc74daf7ec310035f88ee6e0ff37c79d5c3a581
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-02 21:07:55 +0300

    feat: add calculateHash functionality

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,14 @@
+import { readFile } from 'fs/promises'
+import { createHash } from 'crypto'
+
 export const calculateHash = async () => {
-  // Write your code here
+  await readFile(
+    new URL('./files/fileToCalculateHashFor.txt', import.meta.url)
+  ).then((fileBuffer) => {
+    const hash = createHash('sha256').update(fileBuffer)
+    const hex = hash.digest('hex')
+    console.log(hex)
+  })
 }
+
+calculateHash()
