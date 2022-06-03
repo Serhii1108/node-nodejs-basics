2044b83bd1024efdcb7f28dfd5cf81b430066354
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-03 22:36:01 +0300

    feat: add zip/compress file functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,24 @@
+import { createGzip } from 'zlib'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+import { createReadStream, createWriteStream } from 'fs'
+
+const fileToCompressPath = new URL(
+  './files/fileToCompress.txt',
+  import.meta.url
+)
+const archiveFilePath = new URL('./files/archive.gz', import.meta.url)
+
 export const compress = async () => {
-  // Write your code here
+  const pipe = promisify(pipeline)
+
+  const gzip = createGzip()
+  const source = createReadStream(fileToCompressPath)
+  const destination = createWriteStream(archiveFilePath)
+
+  await pipe(source, gzip, destination).catch((err) => {
+    console.error(err)
+  })
 }
+
+compress()
