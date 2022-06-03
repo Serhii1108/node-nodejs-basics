9a1c291b42d01de766328059e03c399d0ca9a56b
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-03 21:52:20 +0300

    feat: add streams/write file functionality

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import { createWriteStream } from 'fs'
+
+const filePath = new URL('files/fileToWrite.txt', import.meta.url)
+
 export const write = async () => {
-  // Write your code here
+  const stream = new createWriteStream(filePath, 'utf-8')
+
+  process.stdin
+    .on('data', (data) => {
+      stream.write(data)
+    })
+    .on('error', (err) => {
+      console.error(err)
+    })
 }
+
+write()
