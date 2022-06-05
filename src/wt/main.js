44b0f2e19d25110c193771cfc83bc2b84201a48a
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-06-05 18:20:39 +0300

    feat: add workers functionality

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,55 @@
-export const performCalculations = async () => {
-  // Write your code here
+import { Worker } from 'worker_threads'
+import { cpus } from 'os'
+
+const filePath = new URL('./worker.js', import.meta.url)
+
+const createWorker = (workerNum) => {
+  return new Promise((res, rej) => {
+    const worker = new Worker(filePath, {
+      workerData: { coresNum: workerNum },
+    })
+
+    worker.on('message', (val) => {
+      res(val)
+    })
+    worker.on('error', rej)
+    worker.on('exit', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Worker stopped with exit code ${code}`))
+      }
+    })
+  })
 }
+
+const DEF_NUM = 10
+const CORES_NUM = cpus().length
+const RESOLVED = 'resolved'
+const ERROR = 'error'
+
+export const performCalculations = async () => {
+  try {
+    console.clear()
+    console.log('Starting...')
+
+    let res = []
+
+    for (let i = 0; i < CORES_NUM; i++) {
+      let data = null
+      try {
+        data = await createWorker(DEF_NUM + i)
+      } catch (err) {
+        console.error(err)
+      }
+      const status = data ? RESOLVED : ERROR
+      res.push({ status, data })
+    }
+
+    console.log('Finished!')
+    console.log('Result:')
+    console.table(res)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+performCalculations()
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,7 +1,16 @@
-// n should be received from main thread
+import { parentPort, workerData, isMainThread } from 'worker_threads'
+
 export const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2)
 
 export const sendResult = () => {
-  // This function sends result of nthFibonacci computations to main thread
+  if (!isMainThread && workerData.coresNum) {
+    parentPort.postMessage(nthFibonacci(workerData.coresNum))
+  } else {
+    console.error(
+      "Error: It's main thread. Task completed but function doesn't work in main thread. Please run main.js"
+    )
+  }
 }
+
+sendResult()
